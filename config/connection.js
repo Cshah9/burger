c1335739619d6b3384c228b7a48821ebb868d5d2
Chirag K Shah <chiragkshah@Chirags-MBP.home>
2017-09-13 11:25:01 -0400

    Updated db config with Heroku ClearDB connection details

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,23 @@
 var mysql = require("mysql");
 
 // Set up our connection information
+// LOCALHOST DB
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
+
+//HEROKU DB:
+
 var connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  user: "b088c8fe6c85a0",
+  password: "6ca2addd",
+  database: "heroku_9bafd60fbce33fd"
 });
 
 // Connect to the database
